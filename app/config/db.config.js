a123ca813d30133e2bd11ea8a9899e4f0a786af9
Leandro Cunha <leandrocunha016@gmail.com>
2021-03-14 18:26:36 -0300

    Set timezone

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,10 +4,21 @@ module.exports = {
   PASSWORD: "admin_12345",
   DB: "tutorials",
   dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
+  define: {
+    underscored: true,
+    freezeTableName: true, //use singular table name
+    timestamps: false, // I do not want timestamp fields by default
   },
+  dialectOptions: {
+    useUTC: false, //for reading from database
+    dateStrings: true,
+    typeCast: function (field, next) {
+      // for reading from database
+      if (field.type === "DATETIME") {
+        return field.string();
+      }
+      return next();
+    },
+  },
+  timezone: "+03:00",
 };
